54a1725572fafded8b564303a33bb409bd74f3dc
wodus5677 <wodus5677@gmail.com>
2019-07-27 01:56:32 +0900

    booklist activity

---
+++ BookcodeKR/app/src/main/java/com/example/bookcodekr/myInfor.java
@@ -0,0 +1,13 @@
+package com.example.bookcodekr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class myInfor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_infor);
+    }
+}
